2732d1ece1b4005228a9a918a6800dfbf5d7fcae
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-02 11:07:41 +0530

    implment card closer button...

--- src/Components/CloseIconWithBg.jsx
+++ src/Components/CloseIconWithBg.jsx
@@ -1,4 +1,4 @@
-export const CloseIconWithBg = ({ handleClose }) => {
+export const CloseIconWithBg = ({ onClick }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,7 +11,7 @@ export const CloseIconWithBg = ({ handleClose }) => {
       strokeLinecap="round"
       strokeLinejoin="round"
       className="bg-[#262626] rounded-full w-[23px] h-[23px] stroke-[3px] p-[3px] cursor-pointer"
-      onClick={handleClose}
+      onClick={onClick}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <line x1="18" y1="6" x2="6" y2="18"></line>
