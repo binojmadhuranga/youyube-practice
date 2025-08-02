cb794ebfe42089a3a03923b6a9629b405469f39b
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-02 06:05:25 +0530

    make some ui adjestment.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className='relative mt-4'>
       <img src={HeroImg} alt="Hero" className='w-[100%]' />
-      <button className='absolute bottom-[3%] left-[25%] bg-white text-black py-2 px-4 rounded-3xl '>Shop the new collection</button>
+            <button className='absolute bottom-[3%] left-[25%] bg-white text-black py-2 px-4 rounded-3xl font-bold'>Shop the new collection</button>
     </div>
   )
 }
