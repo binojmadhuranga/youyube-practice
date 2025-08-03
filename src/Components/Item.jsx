7195040a815f6f287ec9f78b73960a60640b69b1
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-03 10:02:07 +0530

    Add Item component to SideMenu and integrate social media icons

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -5,7 +5,7 @@ export default function ({name,item,noBorder,leftIcon}) {
 
   return (
     <div>
-        
+        <div>{name} </div>
     </div>
   )
 }
--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -1,17 +1,39 @@
 import { sub } from 'motion/react-client';
 import Logo from '../assets/logo.avif';
 import { CloseIcon } from './CloseIcon';
+import Item from './Item';
+import { InstagramIcon } from './InstagramIcon';
+import { TiktokIcon } from './TiktokIcon';
+import { YoutubeIcon } from './YoutubeIcon';
 
 function SideMenu({ isOpen, handleClose }) {
     return (
         <>
-            <div className='fixed bg-black/40 w-[100vw] h-screen top-0'>
+            <div className='fixed bg-black/40 w-[100vw] h-[100vh] top-0 z-20'>
                 <div className='bg-white/90 w-[80%] z-20 fixed top-0 right-0'>
                     <div className='flex items-center justify-between p-4 min-h-[10vh]'>
                         <img src={Logo} alt="logo" className='h-[20px] w-[80px] ' />
                         <CloseIcon onClick={handleClose} />
                     </div>
-                    <div className='flex flex-col items-start justify-start h-[90vh] mt-2.5 px-4'>
+                    <div className='flex flex-col items-start justify-start h-[90vh] mt-2.5 px-4 bg-white/90 z-20'>
+                        {
+                            items.map((item) => (
+                                <Item
+                                    key={item.name}
+                                    name={item.name}
+                                    items={item.subItem}
+                                />
+
+                            ))
+                        }
+                        <Item name={"Account"}/>
+                        <div className='flex items-center gap-2'>
+                            <InstagramIcon/>
+                            <TiktokIcon/>
+                            <YoutubeIcon/>
+
+                        </div>
+
 
                     </div>
 
@@ -28,7 +50,7 @@ export default SideMenu
 const items = [
     {
         name: "12 days of Christmas"
-       
+
     },
     {
         name: "Men",
