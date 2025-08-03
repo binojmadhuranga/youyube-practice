56b5749b12957552e6418e367c6cc724bc95df91
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2025-08-03 08:05:24 +0530

    Refactor SideMenu component to include logo and close icon functionality

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import Logo from '../assets/logo.avif';
+import { CloseIcon } from './CloseIcon';
 
-function SideMenu() {
-  return (
-    <>
-    <div className='fixed bg-black/40 w-[100vw] h-screen top-0'></div>
-    
-    
-    
-    </>
-  )
+function SideMenu({ isOpen, handleClose }) {
+    return (
+        <>
+            <div className='fixed bg-black/40 w-[100vw] h-screen top-0'>
+                <div className='bg-white/90 w-[80%] z-20 fixed top-0 right-0'>
+                    <div className='flex items-center justify-between p-4 min-h-[10vh]'>
+                        <img src={Logo} alt="logo" className='h-[20px] w-[80px] ' />
+                        <CloseIcon onClick={handleClose} />
+                    </div>
+
+
+
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default SideMenu
